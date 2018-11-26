e5bb9d16ff4a57f021572e7363eb62fd3155a0ee
MD-Levitan <ovsyanka@protonmail.com>
2018-11-26 16:14:53 +0300

    Add morse decoder

--- Decoder/decoder.py
+++ Decoder/decoder.py
@@ -4,12 +4,13 @@ import sys
 
 from base import Base
 from permutation import Permutation
+from morse import Morse
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Decoder')
     parser.add_argument('--data', help='data to decode')
-    decode_list = Base.algorithms + Permutation.algorithms + ["all"]
+    decode_list = Base.algorithms + Permutation.algorithms + Morse.algorithms + ["all"]
     parser.add_argument('--decode', help='Specify the decode mode.', default="all", choices=decode_list)
 
     args = parser.parse_args()
